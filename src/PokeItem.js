72d7b302831beac5c7a784a25f1acbe43b20befd
Scott Harlan <seharlan@gmail.com>
2020-02-13 10:06:19 -0800

    tests pass

--- src/PokeItem.js
+++ src/PokeItem.js
@@ -9,7 +9,7 @@ export default class App extends Component {
             <span className="type" style={{backgroundColor:this.props.item.color_1}}>{this.props.item.type_1}</span>
             <span>HP: {this.props.item.hp}</span>
           </div>
-          <img src={this.props.item.url_image} alt={this.props.item.pokemon} style={borderStyle} />
+          <img src={this.props.item.url_image} alt={this.props.item.pokemon} />
           <div>
             <span>A: {this.props.item.attack}</span>
             <span>D: {this.props.item.defense}</span>
